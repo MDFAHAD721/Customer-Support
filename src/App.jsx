a351be8015ffa9dbf6ee0d0d054a73a0364eb8af
Sheikh Fahad <sheikhfahad721@gmail.com>
2026-07-01 13:13:09 +0600

    added cards section with responsive

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,16 @@ function App() {
     <>
       <Navbar></Navbar>
       <Hero></Hero>
-      <div className="w-11/12 mx-auto grid grid-cols-5 gap-3 mt-10">
-        <div className="col-span-4">
+      <div className="w-11/12 mx-auto grid grid-cols-1 md:grid-cols-5 gap-3 mt-10">
+        <div className="col-span-1 md:col-span-4 order-2 md:order-1">
           <CustomerProblem ticket={promise}></CustomerProblem>
         </div>
-        <div className="px-2">
+        <div className="px-2 order-1 md:order-2">
           <div className="mb-2">
             <h1 className="text-2xl font-bold">Task Status</h1>
-            <p className="text-gray-500">Select a ticket to add to Task Status.</p>
+            <p className="text-gray-500">
+              Select a ticket to add to Task Status.
+            </p>
           </div>
           <div>
             <h1 className="text-2xl font-bold">Resolved Task</h1>
--- src/customersProblems/CustomerProblem.jsx
+++ src/customersProblems/CustomerProblem.jsx
@@ -1,53 +1,55 @@
 import { use } from "react";
 import { MdOutlineDateRange } from "react-icons/md";
 const CustomerProblem = ({ ticket }) => {
-
   const ticketsData = use(ticket);
   console.log(ticket.value);
   return (
-    
     <div>
       <h1 className="text-2xl font-bold mb-4">Customer Tickets</h1>
 
-     <div className="grid grid-cols-2 gap-5">
-       {
-        ticketsData.map((customer) =>(
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        {ticketsData.map((customer) => (
           <div>
-        <div key={customer.id} className=" bg-white p-3 rounded-xl shadow-md flex flex-col justify-between gap-5 h-full">
-          <div className="flex justify-between items-center">
-            <h1 className="text-xl font-semibold flex-1">
-            {customer.title}
-            </h1>
-            <div className={`px-3 py-1 font-semibold rounded-3xl flex items-center gap-2 
+            <div
+              key={customer.id}
+              className=" bg-white p-3 rounded-xl shadow-md flex flex-col justify-between gap-5 h-full"
+            >
+              <div className="flex justify-between items-center">
+                <h1 className="text-xl font-semibold flex-1">
+                  {customer.title}
+                </h1>
+                <div
+                  className={`px-3 py-1 font-semibold rounded-3xl flex items-center gap-2 
               ${customer.status === "Open" ? "open-green-bg" : "progress-bg"}
-              `}>
-              <span className="w-3 h-3 rounded-2xl bg-black"></span>
-              <p>{customer.status}</p>
-            </div>
-          </div>
-          <p className="text-gray-500">
-          {customer.description}
-          </p>
+              `}
+                >
+                  <span
+                    className={`w-3 h-3 rounded-2xl ${customer.status === "Open" ? "open-dot-green" : "progress-dot"}`}
+                  ></span>
+                  <p className={customer.status === "Open" ? "open-green" : "progress-font"}>
+                    {customer.status}
+                  </p>
+                </div>
+              </div>
+              <p className="text-gray-500">{customer.description}</p>
 
-          <div className="flex justify-between items-center">
-            <div className="flex gap-3">
-              <p className="font-semibold text-gray-500">{customer.id}</p>
-              <p className="font-semibold text-gray-500">{customer.priority}</p>
-            </div>
-            <div className="flex items-center">
-              <p className="mr-3 text-gray-500">{customer.assignee}</p>
-              <MdOutlineDateRange className="text-3xl text-gray-500" />
-              <p className="text-gray-500">{customer.date}</p>
+              <div className="flex justify-between items-center">
+                <div className="flex items-center gap-3">
+                  <p className="font-semibold text-gray-500">{customer.id}</p>
+                  <p className="font-semibold text-gray-500">
+                    {customer.priority}
+                  </p>
+                </div>
+                <div className="flex items-center">
+                  <p className="mr-3 text-gray-500">{customer.assignee}</p>
+                  <MdOutlineDateRange className="text-3xl text-gray-500" />
+                  <p className="text-gray-500">{customer.date}</p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
-        ))
-      }
-
-     </div>
-
-      
     </div>
   );
 };
